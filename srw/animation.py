5ca790d4c8103f2c61258233103e0ac71b8ffc87
Simon Walker <s.r.walker101@googlemail.com>
2014-05-16 11:08:21 +0100

    Adds documentation to animation module

--- srw/animation.py
+++ srw/animation.py
@@ -1,3 +1,8 @@
+'''
+Helper classes for matplotlib animations
+'''
+
+
 import time
 import matplotlib.pyplot as plt
 import logging
@@ -5,12 +10,26 @@ from contextlib import contextmanager
 logging.basicConfig(level=logging.DEBUG)
 
 class Plotter(object):
+    '''Handles auto updating a plot, currently with a single line on it.
+
+    Example usage:
+
+    >>> ax = fig.add_subplot(111)
+    >>> pax = Plotter(ax)
+    >>>
+    >>> while True:
+    >>>     pax.update(xdata, ydata, 'r.')
+    >>>     time.sleep(10)
+    '''
     def __init__(self, ax, autoscale=True):
         self.ax = ax
         self.line = None
         self.autoscale = autoscale
 
     def update(self, x, y, *args, **kwargs):
+        ''' Update the data on the attached axis instance.
+
+        If a line is not present, create one '''
         if self.line:
             self.update_line(x, y, *args, **kwargs)
         else:
@@ -28,6 +47,31 @@ class Plotter(object):
         self.line = self.ax.plot(x, y, *args, **kwargs)[0]
 
 class AutoUpdater(object):
+    '''
+    Class to make a long running plot easier.
+
+    Construct an object before making any matplotlib figures etc., then supply
+    an update function to call on each tick. This function should perform any
+    data analysis required to update the plot.
+
+    Example usage:
+
+    >>> au = AutoUpdater(sleep_time=10)
+    >>> fig, ax = plt.subplots(1, 1)
+    >>> pax = Plotter(ax)
+    >>>
+    >>> def update_plot(ax):
+    >>>     pax.update(xdata, ydata)
+    >>>
+    >>> # Start the rendering loop
+    >>> au.run(update_plot, pax)
+
+    The class takes these parameters:
+
+    * `sleep_time`: time to sleep before each tick. Inverse frame rate
+    * `repeat`:     whether to repeat or not. Set to false to just plot once
+                    and return to normal matplotlib behaviour
+    '''
     def __init__(self, sleep_time, repeat=True):
         self.sleep_time = sleep_time
         self.repeat = repeat
@@ -52,5 +96,6 @@ class AutoUpdater(object):
 
 @contextmanager
 def auto_updater(*args, **kwargs):
+    ''' Create an AutoUpdater instance with a context '''
     au = AutoUpdater(*args, **kwargs)
     yield au
